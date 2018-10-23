1fe57de2fdc8e8c8858cd3cebf78b94f16604b01
Chyld Medford <chyld.medford@gmail.com>
2018-10-23 13:46:09 -0700

    skeleton code

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-const Rot = require('./src/rot');
-const defaults = require('./config/defaults');
+// entry point
 
-const n = process.env.ROT || defaults.n;
-const plain_text = process.argv[2] || defaults.text;
-const cipher_text = Rot.encode(plain_text, n);
-
-console.log('plain:', plain_text, 'cipher:', cipher_text, 'n:', n);
+console.log("plain:", plain_text, "cipher:", cipher_text, "n:", n);
